282f073ca17dd1f03b36fb04d6e91301031134e3
Lisa <sulinwood@gmail.com>
2022-04-26 22:47:26 +0800

    fix PWA

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { InitProvider } from "./contexts/InitContext";
 import reportWebVitals from "./reportWebVitals";
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import "./i18n";
 import { URLProvider } from "./contexts/URLContext";
 import { FunctionProvider } from "./contexts/FunctionContext";
@@ -25,6 +26,12 @@ ReactDOM.render(
      </React.StrictMode>,
      document.getElementById("root")
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+// serviceWorkerRegistration.unregister();
+serviceWorkerRegistration.register()
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
